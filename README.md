1a7e0ea68354ff234dca0d48b79c5d892960dd84
Dajnaelhun <dajnaelhun@gmail.com>
2020-06-07 22:17:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JAVAalap
+ 
---
+++ java.js
@@ -0,0 +1,63 @@
+/*let ertek=10
+for (let i=0; i<ertek; i++)
+    {console.log(i)};
+
+let ertek2=0
+for (let x=10; x>ertek2; x = x-1)
+{console.log(x)};
+
+let szamlalo=21
+for (let hely = 0; hely<szamlalo; hely = hely+1)
+{console.log(hely)}*/
+
+/*let printNumbers = (n) => {
+ for (let i=0; i<n; i++){
+ console.log(i);
+ return (printNumbers)
+ }}*/
+
+
+let printNumbers20 = (n, h) => 
+   { {for (let i=1; i<=n; i++)
+        {console.log(i)}
+    }
+    {for (let g=1; g<=h; g++)
+        {console.log(g)}}}
+   console.log(printNumbers20(1,20))
+
+
+ let printnumber15 = (a, b) =>
+   {{for (let sz=1; sz<=a; sz++)
+    {console.log(sz)}}
+    {for (let sza=1; sza<=b; sza++)
+        {console.log(sza)}}}
+   console.log(printnumber15(1,15))
+
+
+let name = "Dani"
+  let greet = (name) => {
+   return('Hello, '+ name);
+      };
+      console.log(greet(name))
+
+let tomb = [0, 3, 6, 7, 9]
+      let printtomb = (tomb) =>
+      {
+       for (let z=0; z<tomb.length; z++)
+       {
+           console.log(tomb[z]);
+       }
+      };
+      console.log(printtomb(tomb))
+
+
+
+
+
+
+
+
+
+
+
+
